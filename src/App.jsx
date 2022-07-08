eea9024db760797f973ed0add6eb1e993de025ae
mahzze <zanzinzzo@gmail.com>
2022-07-08 00:34:58 -0300

    identation fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,48 +9,48 @@ import SearchIcon from './search.svg'
 const URL_OF_API = 'http://www.omdbapi.com?apikey=4ffcc20c'
 
 const App = () =>{
-	const [searchText, setSearchText] = useState([])
-	const [movies, setMovies] = useState([])
+   const [searchText, setSearchText] = useState([])
+   const [movies, setMovies] = useState([])
 
-	const searchMovies = async(title) => {
-		const response = await fetch(`${URL_OF_API}&s=${title}`)
-		const data = await response.json()
+   const searchMovies = async(title) => {
+   const response = await fetch(`${URL_OF_API}&s=${title}`)
+   const data = await response.json()
 
-		setMovies(data.Search)
-	}
+   setMovies(data.Search)
+   }
 
-	return (
-		<div className="app">
-			<h1>Movies stupid app </h1>
+   return (
+      <div className="app">
+      <h1>Movies stupid app </h1>
 
-			<div className="search">
-			<input 
-			placeholder="Search for Movies" 
-			value={searchText}
-			onChange={e => setSearchText(e.target.value)} 
-			/>
+      <div className="search">
+         <input 
+         placeholder="Search for Movies" 
+         value={searchText}
+         onChange={e => setSearchText(e.target.value)} 
+         />
 
-			<img 
-			src={SearchIcon}
-			alt="search"
-			onClick={() => searchMovies(searchText)}
-			/>
-		</div>
+         <img 
+         src={SearchIcon}
+         alt="search"
+         onClick={() => searchMovies(searchText)}
+         />
+      </div>
 
-		{movies?.length > 0
-		? (
-			<div className="container">
-				{movies.map(movie => <MovieCard movie={movie}/>)}
-			</div>
-		) : (
-			<div className="empty">
-				<h2>No movies found</h2>
-			</div>
-		)
-		}
-		</div>
+      {movies?.length > 0
+      ? (
+         <div className="container">
+         {movies.map(movie => <MovieCard movie={movie}/>)}
+         </div>
+      ) : (
+         <div className="empty">
+            <h2>No movies found</h2>
+         </div>
+      )
+      }
+      </div>
 
-	)
+   )
 }
 
 export default App
--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const MovieCard = ({ movie }) =>{
-    
+
     return (
         <div className="movie">
             <div>
