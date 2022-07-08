fca528cf1038e8ef56f8c5198baa622c508adef4
mahzze <zanzinzzo@gmail.com>
2022-07-08 00:15:39 -0300

    completed, but will be tweaked a bit for studies

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,56 @@
 import { useState } from "react"
 import './App.css'
+import MovieCard from "./MovieCard"
+import SearchIcon from './search.svg'
+
 // omdb is an API with data about movies, the free key(like the one used here) lasts for 1000 days
 // key: 4ffcc20c
 
 const URL_OF_API = 'http://www.omdbapi.com?apikey=4ffcc20c'
 
 const App = () =>{
-    
+	const [searchText, setSearchText] = useState([])
+	const [movies, setMovies] = useState([])
 
-    const searchMovie = async(title) => {
-        const response = await fetch(`${URL_OF_API}&s=${title}`)
-        const data = await response.json()
+	const searchMovies = async(title) => {
+		const response = await fetch(`${URL_OF_API}&s=${title}`)
+		const data = await response.json()
 
-        console.log(data.Search)
-    }
-    
-    return (
-        <>
-            <input type="text" id="text"
-            />
-        </>
-    )
+		setMovies(data.Search)
+	}
+
+	return (
+		<div className="app">
+			<h1>Movies stupid app </h1>
+
+			<div className="search">
+			<input 
+			placeholder="Search for Movies" 
+			value={searchText}
+			onChange={e => setSearchText(e.target.value)} 
+			/>
+
+			<img 
+			src={SearchIcon}
+			alt="search"
+			onClick={() => searchMovies(searchText)}
+			/>
+		</div>
+
+		{movies?.length > 0
+		? (
+			<div className="container">
+				{movies.map(movie => <MovieCard movie={movie}/>)}
+			</div>
+		) : (
+			<div className="empty">
+				<h2>No movies found</h2>
+			</div>
+		)
+		}
+		</div>
+
+	)
 }
 
 export default App
